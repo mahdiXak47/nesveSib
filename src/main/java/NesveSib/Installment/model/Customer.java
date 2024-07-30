6b268ea7cd8f1182bd38fefa41ddda8c82d3f0dc
mahdi <mahdiamcfc@gmail.com>
2024-07-31 01:41:17 +0330

    creating customer model

---
+++ src/main/java/NesveSib/Installment/model/Customer.java
@@ -0,0 +1,44 @@
+package NesveSib.Installment.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+public class Customer {
+
+    private String nationalId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String fathersName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private boolean emailVerified;
+    private boolean phoneVerified;
+    private Date dateOfBirth;
+    private String encryptedPassword;
+    private String relativePhoneNumber;
+    private String secondRelativePhoneNumber;
+
+
+    @Override
+    public String toString() {
+        return "nationalId is " + nationalId + ", firstName is " + firstName + ", lastName is " + lastName
+                + "\n, username is " + username
+                + "\n, fathersName is " + fathersName
+                + "\n, email is " + email
+                + "\n, phone number is " + phoneNumber
+                + "\n, address is " + address
+                + "\n, emailVerified is " + emailVerified
+                + "\n, phoneVerified is " + phoneVerified
+                + "\n, dateOfBirth is " + dateOfBirth
+                + "\n, encryptedPassword is " + encryptedPassword
+                + "\n, relativePhoneNumber is " + relativePhoneNumber
+                + "\n, secondRelativePhoneNumber is " + secondRelativePhoneNumber;
+    }
+}
